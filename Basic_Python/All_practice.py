f1f3f6d693be8b72071dab58ae3c171e93870872
sabujghorai <ghoraisabuj4@gmail.com>
2026-06-18 22:16:19 +0530

    updated the python file

--- Basic_Python/All_practice.py
+++ Basic_Python/All_practice.py
@@ -1,28 +1,28 @@
 # Print a multiplication table for a number.
 
-# a = int(input("Enter the number for multiplication table :"))
+a = int(input("Enter the number for multiplication table :"))
 
-# for i in range(1,11):
-#     print(a,"x", i, "=" ,a*i)
+for i in range(1,11):
+    print(a,"x", i, "=" ,a*i)
 
 
 # Check if a number is prime.
 
-# number = (int(input("Enter the number to check the number is prime or not :")))
+number = (int(input("Enter the number to check the number is prime or not :")))
 
-# if number <= 1 :
-#     print("Not prime")
+if number <= 1 :
+    print("Not prime")
 
-# else:
-#     is_prime = True
-#     for i in range(2 , number):
-#         if number % i == 0:
-#             is_prime = False
+else:
+    is_prime = True
+    for i in range(2 , number):
+        if number % i == 0:
+            is_prime = False
 
-#     if is_prime:
-#         print("Prime")
-#     else:
-#         print("not prime")
+    if is_prime:
+        print("Prime")
+    else:
+        print("not prime")
 
 
 # Find the factorial of a number.
