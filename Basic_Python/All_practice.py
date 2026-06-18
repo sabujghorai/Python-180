d24d3c110ceb901b4624e72b3d1d2d4b0e476ee6
sabujghorai <ghoraisabuj4@gmail.com>
2026-06-18 21:49:53 +0530

    updated the py file

--- Basic_Python/All_practice.py
+++ Basic_Python/All_practice.py
@@ -1,6 +1,16 @@
 # Print a multiplication table for a number.
 
-a = int(input("Enter the number for multiplication table :"))
+# a = int(input("Enter the number for multiplication table :"))
 
-for i in range(1,11):
-    print(a,"x", i, "=" ,a*i)
+# for i in range(1,11):
+#     print(a,"x", i, "=" ,a*i)
+
+
+# Check if a number is prime.
+
+prime = int(input("Enter the number to check the number is prime or not : "))
+
+if(prime // 1 != 0 and prime // prime != 0):
+    print("The number is prime")
+else:
+    print("The number is not prime..")
