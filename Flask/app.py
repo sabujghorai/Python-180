98c6b8060f0b78f74650ecad391cf6d7dd4483d9
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-14 12:58:36 +0530

    updated the app.py

--- Flask/app.py
+++ Flask/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello world how are you "
+    return "hello world ! how are you "
 
 if __name__ == "__main__" :
     app.run(debug=True)
