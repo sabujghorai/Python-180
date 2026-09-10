1990f7dc0098c2f07ede0b7d427772fa4484378e
sabujghorai <ghoraisabuj4@gmail.com>
2026-09-10 22:40:36 +0530

    updated the pythonproblem.py file

--- Basic_Python/pythonproblem.py
+++ Basic_Python/pythonproblem.py
@@ -1,34 +1,34 @@
-# # Write a program that checks if a number is even or odd.
-# a = int(input("Enter a nimber :"))
-# if a%2 == 0:
-#     print("The number is even")
-# else:
-#     print("The number is odd")
+# Write a program that checks if a number is even or odd.
+a = int(input("Enter a nimber :"))
+if a%2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
 
 
-# # Take a list of numbers and return their sum
-# number = [1,3,4,2,5,7,9]
-# total = sum(number)
-# print("sum is = ",total)
+# Take a list of numbers and return their sum
+number = [1,3,4,2,5,7,9]
+total = sum(number)
+print("sum is = ",total)
 
 
-# # Given a list, find the largest number.
-# list = [20,30,40,50,77,11,85,75,102]
-# find = max(list)
-# print("The maximum number is :",find)
+# Given a list, find the largest number.
+list = [20,30,40,50,77,11,85,75,102]
+find = max(list)
+print("The maximum number is :",find)
 
 
-# # Reverse a String
-# string = "Hello world"
+# Reverse a String
+string = "Hello world"
 
-# char_text = ""
-# for char in string:
-#     char_text = char+char_text
+char_text = ""
+for char in string:
+    char_text = char+char_text
 
-# print("The reversed string is :- ",char_text)
+print("The reversed string is :- ",char_text)
 
-# string1 = "good morning"
-# print("the reversed string is :",string1[::-1])
+string1 = "good morning"
+print("the reversed string is :",string1[::-1])
 
 value = (input("Enter a number or string :"))
 for i in range(value):
